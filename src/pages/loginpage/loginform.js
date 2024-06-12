9ea93eb0c6e82d3692b88c957af520bf975af09d
HamzaCode786 <ahmadbrothers13@gmail.com>
2024-06-12 17:49:33 +0500

    yes

--- src/pages/loginpage/loginform.js
+++ src/pages/loginpage/loginform.js
@@ -25,14 +25,14 @@ const loginform = () => {
 
           <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
 
-            <h3 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px', color:'white', fontSize:'35px'}}>Log in</h3>
+            <h3 className="fw-normal mb-3 ps-5 pb-3" style={{letterSpacing: '1px', color:'white', fontSize:'40px'}}>Log in</h3>
 
             <MDBInput wrapperClass='mb-4 mx-5  email_address' label='Email address' id='formControlLg' type='email' size="lg"/>
             <MDBInput wrapperClass='mb-4 mx-5 password_o' label='Password' id='formControlLg' type='password' size="lg"/>
 
             <MDBBtn className="mb-4 px-5 mx-5 lb" size='lg'>Login</MDBBtn>
-            <p className="small mb-5 pb-lg-3 ms-5"><a class="text-muted" href="#!">Forgot password?</a></p>
-            <p className='ms-5'>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
+            <p className="small mb-5 pb-lg-3 ms-5"><a class="text-muted1" href="#!">Forgot password?</a></p>
+            <p className='ms-5'>Don't have an account? <a href="#!" class="link-info1">Register here</a></p>
 
           </div>
 
