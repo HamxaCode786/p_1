d3a0a5d516ec0abfd4dd5c131c8a813cd72da3a3
HamzaCode786 <ahmadbrothers13@gmail.com>
2024-06-13 18:02:18 +0500

    yes

--- src/pages/startsale/sale.js
+++ src/pages/startsale/sale.js
@@ -1,20 +1,27 @@
-
-import React, { useState } from 'react';
+import React from 'react';
+import Logo_listing from '../../assets/image/drawerimage.png';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import CampaignOutlinedIcon from '@mui/icons-material/CampaignOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
+import EnhancedEncryptionOutlinedIcon from '@mui/icons-material/EnhancedEncryptionOutlined';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import AppBar from '@mui/material/AppBar';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 
 const Sale = () => {
@@ -29,32 +36,33 @@ const Sale = () => {
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
-          },
-        }}
+            zIndex: '-1',
+            height: '100%',
+            position:'static',
+            backgroundColor: 'rgba(0, 0, 0, 0.02)',
+        }}}
         variant="permanent"
         anchor="left"
       >
         <Toolbar />
-        <Divider />
+        
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        <div Class="drawer_logo">
+          <img class="drawer_img"  src={Logo_listing}/>
+        </div>
+          {['Home', 'Profile', 'Add New Listing', 'My Add', 'Favourites', 'Inbox','Billings','Password','Logout'].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {index === 0 && <InboxIcon/>}
+                {index === 1 && <HomeTwoToneIcon/>}
+                {index === 2 && <AccountCircleOutlinedIcon/>}
+                {index === 3 && <CampaignOutlinedIcon/>}
+                {index === 4 && <StarBorderOutlinedIcon/>}
+                {index === 5 && <MapsUgcOutlinedIcon/>}
+                {index === 6 && <LocalAtmOutlinedIcon/>}
+                {index === 7 && <EnhancedEncryptionOutlinedIcon/>}
+                {index === 8 && <LogoutRoundedIcon/>}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
@@ -67,24 +75,11 @@ const Sale = () => {
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          
-        </Typography>
+        
       </Box>
+      
     </Box>
     </div>
   );
 }
-export default Sale
-
-
+export default Sale 
